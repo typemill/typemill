3256e2b682fbdc24addbc0f7ebc1582d84e0a3c4
trendschau <trendschau@gmail.com>
2022-02-12 21:37:57 +0100

    Composer Updates and update Version2

--- system/Settings.php
+++ system/Settings.php
@@ -84,7 +84,7 @@ class Settings
 			'editor'								=> 'visual',
 			'formats'								=> ['markdown', 'headline', 'ulist', 'olist', 'table', 'quote', 'notice', 'image', 'video', 'file', 'toc', 'hr', 'definition', 'code', 'shortcode'],
 			'contentFolder'							=> 'content',
-			'version'								=> '1.5.2',
+			'version'								=> '1.5.3',
 			'setup'									=> true,
 			'welcome'								=> true,
 			'images'								=> ['live' => ['width' => 820], 'thumbs' => ['width' => 250, 'height' => 150]],
