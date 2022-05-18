2932b457e9654cc5fb6ec8c8bb531411b9841183
matbgn <mat.gh@bqn.ch>
2022-05-19 00:13:06 +0200

    Reduce impacted files before merge

--- init_content.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "**************************************************************************"
-echo "! Content directory is empty. It will be initialized with default content."
-echo "**************************************************************************"
-cp -R /var/www/html/content.orig/* /var/www/html/content/
