346c61a46cec0af7c6307a730607dbe8ed641523
trendschau <trendschau@gmail.com>
2024-02-07 16:52:21 +0100

    v2.1.2 fix display errors

--- system/typemill/system.php
+++ system/typemill/system.php
@@ -48,7 +48,9 @@ $timer['start'] = microtime(true);
 * HIDE ERRORS BY DEFAULT    *
 ****************************/
 
-ini_set('display_errors', 0);
+$display_errors = 0;
+
+ini_set('display_errors', $display_errors);
 ini_set('display_startup_errors', 0);
 error_reporting(E_ALL);
 
@@ -67,8 +69,8 @@ $settings = $settingsModel->loadSettings();
 ****************************/
 if(isset($settings['displayErrorDetails']) && $settings['displayErrorDetails'])
 {
-	ini_set('display_errors', 1);
-#	ini_set('display_startup_errors', 1);	
+	$display_errors = 1;
+	ini_set('display_errors', $display_errors);
 }
 
 
@@ -354,7 +356,7 @@ $app->addRoutingMiddleware();
 $errorMiddleware = new ErrorMiddleware(
 	$app->getCallableResolver(),
 	$app->getResponseFactory(),
-	true,
+	$display_errors,
 	false,
 	false
 );
