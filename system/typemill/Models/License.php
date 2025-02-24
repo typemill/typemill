1ceb4870924fa1814c9848ea2226701ba7b1160d
trendschau <trendschau@gmail.com>
2025-02-24 13:37:59 +0100

    Fix Typo in License Test Call

--- system/typemill/Models/License.php
+++ system/typemill/Models/License.php
@@ -496,7 +496,7 @@ class License
 			$curl = curl_init($url);
 			if (defined('CURLSSLOPT_NATIVE_CA') && version_compare(curl_version()['version'], '7.71', '>='))
 			{
-				curl_setopt($ch, CURLOPT_SSL_OPTIONS, CURLSSLOPT_NATIVE_CA);  
+				curl_setopt($curl, CURLOPT_SSL_OPTIONS, CURLSSLOPT_NATIVE_CA);  
 			}			
 			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($curl, CURLOPT_POST, true);
